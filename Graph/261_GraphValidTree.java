bd27882d7376f7893e516d44d3b9a49aa110a435
Saikanth reddy Meda <meda.s@husky.neu.edu>
2021-09-08 21:46:56 -0400

    graph disjointset solition added

--- Graph/261_GraphValidTree.java
+++ Graph/261_GraphValidTree.java
@@ -49,3 +49,73 @@ class Solution {
         return true;
     }
 }
+
+// Solution Using DisjointSet
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        DisjointSet ds = new DisjointSet(n);
+        for (int[] edge : edges) {
+            if (ds.isInSameGroup(edge[0], edge[1]))
+                return false;
+            ds.union(edge[0], edge[1]);
+        }
+        return ds.components == 1 ? true : false;
+    }
+    
+    class DisjointSet {
+        private int[] weights; // Used to store weights of each nodes 
+        private int[] parents;
+        int components;
+
+        public void union(int a, int b) {
+            int rootA = find(a);
+            int rootB = find(b);
+            // If both a and b have same root, i.e. they both belong to the same set, hence we don't need to take union
+            if (rootA == rootB) return;
+            
+            this.components--;
+            // Weighted union
+            if (this.weights[rootA] > this.weights[rootB]) {
+                // In case rootA is having more weight
+                // 1. Make rootA as the parent of rootB
+                // 2. Increment the weight of rootA by rootB's weight
+                this.parents[rootB] = rootA;
+                this.weights[rootA] += this.weights[rootB];
+            } else {
+                // Otherwise
+                // 1. Make rootB as the parent of rootA
+                // 2. Increment the weight of rootB by rootA's weight
+                this.parents[rootA] = rootB;
+                this.weights[rootB] += this.weights[rootA];
+            }
+        }
+
+        public int find(int a) {
+            // Traverse all the way to the top (root) going through the parent nodes
+            while (a != this.parents[a]) {
+                // Path compression
+                // a's grandparent is now a's parent
+                this.parents[a] = this.parents[parents[a]];
+                a = this.parents[a];
+            }
+            return a;
+        }
+
+        public boolean isInSameGroup(int a, int b) {
+            // Return true if both a and b belong to the same set, otherwise return false
+            return find(a) == find(b);
+        }
+
+        // Initialize weight for each node to be 1
+        public DisjointSet(int N) {
+            this.parents = new int[N + 1];
+            this.weights = new int[N + 1];
+            this.components = N;
+            // Set the initial parent node to itself
+            for (int i = 1; i <= N; ++i) {
+                this.parents[i] = i;
+                this.weights[i] = 1;
+            }
+        }
+    }
+}
